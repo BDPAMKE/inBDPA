68d37978cc2da48aa04d431e39caf86d4fec83e2
Amina <100482564+ItsAminah@users.noreply.github.com>
2023-07-15 15:38:53 +0000

    added captcha

--- source/routes/login.js
+++ source/routes/login.js
@@ -34,58 +34,7 @@ router.post('/', async(req, res, next) => {
         );
     };
 
-    (function(){
-      const fonts = ["cursive"];
-      let captchaValue = "";
-      function gencaptcha()
-      {
-          let value = btoa(Math.random()*1000000000);
-          value = value.substr(0,5 + Math.random()*5);
-          captchaValue = value;
-      }
-  
-      function setcaptcha()
-      {
-          let html = captchaValue.split("").map((char)=>{
-              const rotate = -20 + Math.trunc(Math.random()*30);
-              const font = Math.trunc(Math.random()*fonts.length);
-              return`<span
-              style="
-              transform:rotate(${rotate}deg);
-              font-family:${font[font]};
-              "
-             >${char} </span>`;
-          }).join("");
-          document.querySelector(".login_form #captcha .preview").innerHTML = html;
-      }
-  
-      function initCaptcha()
-      {
-          document.querySelector(".login_form #captcha .captcha_refersh").addEventListener("click",function(){
-              gencaptcha();
-              setcaptcha();
-          });
-  
-          gencaptcha();
-          setcaptcha();
-      }
-      initCaptcha();
-  
-      document.querySelector(".login_form .form_button").addEventListener("click",function(){
-          let inputcaptchavalue = document.querySelector(".login_form #captcha input").value;
-  
-          if (inputcaptchavalue === captchaValue) 
-          {
-              // swal("","Log in","success");
-              alert("Log in success");
-          }
-          else
-          {
-              // swal("Invalid Captcha");
-              alert("Invalid Captcha");
-          }
-      });
-  })();
+    
   
     // A function that converts a string of hexadecimal digits into an array of
     // bytes (you should verify that the string is hex first!)
