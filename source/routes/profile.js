7b7343f8d71d1204a2cce7f4aba60a76c45e4da1
gabbilol <123832029+gabbilol@users.noreply.github.com>
2023-07-15 16:23:33 +0000

    commit changes

--- source/routes/profile.js
+++ source/routes/profile.js
@@ -10,13 +10,14 @@ router.get('/', function(req, res, next) {
  // this in you route 
  const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/mkeadmin' //- where the URL is whatever Get RestAPI Request  you are calling
  const token = process.env.BEARER_TOKEN;
-
+console.log ("before call")
   //########################################## 
  //This function will take the two variables and pass them to the Get RestAPI call 
   myGetRestCall.getWithBearerToken(url, token)
 .then(data => {
   //data.users.forEach(element => element.gravatar=crypto.createHash('md5').update(element.email).digest("hex"));
-  console.log('data ',data.user)
+  var results = data
+  console.log('data ',results)
   res.render('profile', { title: 'User Profile', resultarray:data.user});
   } 
 )
