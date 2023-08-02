dea5791326821d337b23dd1ff8d33a6b555d5dc0
Alan Sudhan <alanjohn.sudhan@gmail.com>
2023-08-01 22:58:49 -0500

    Working Opportunity Page

--- source/app.js
+++ source/app.js
@@ -17,7 +17,6 @@ var adminRouter = require('./routes/admin');
 var connectcountRouter = require('./routes/connectcount');
 var profileedit = require('./routes/profileedit');
 var opportunitiesRouter = require('./routes/opportunities');
-var opportunityPageRouter = require('./routes/opportunityPage');
 var myOpportunitiesRouter = require('./routes/myOpportunities');
 var profileRouter = require('./routes/profile');
 var app = express();
@@ -45,7 +44,6 @@ app.use('/admin', adminRouter);
 app.use('/connectcount',connectcountRouter);
 app.use('/profileedit',profileedit);
 app.use('/opportunities', opportunitiesRouter);
-app.use('/opportunityPage', opportunityPageRouter);
 app.use('/myOpportunities', myOpportunitiesRouter)
 app.use('/profile', profileRouter);
 // catch 404 and forward to error handler
