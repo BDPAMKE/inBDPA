aca9af7f72e786a07d8f7794ac88e4a9dfe866fe
RahmanMalik10 <100482571+RahmanMalik10@users.noreply.github.com>
2023-08-05 18:01:47 +0000

    added role to header

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const auth = require("../middleware/verifytoken");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'InBDPA Home' });
+router.get('/',auth, function (req, res, next) {
+  const role=res.locals.role;
+  const id=res.locals.id;
+  const name=res.locals.name;
+  res.render('index', { title: 'InBDPA Home',role:role,id:id,name:name });
 });
 
 module.exports = router;
