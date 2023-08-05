6874ae380c4aa3fff1f9af14710dfac5671c55f8
kempensp <kempensp@milwaukee.k12.wi.us>
2023-08-05 17:38:34 +0000

    Profile goes to username

--- source/routes/profile.js
+++ source/routes/profile.js
@@ -27,30 +27,59 @@ async function patchRequest(url, token, data) {
 
 
 
+
+
 /* GET users profile. */
 router.get('/', function(req, res, next) {
- // this in you route 
- const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/' + global.userID //- where the URL is whatever Get RestAPI Request  you are calling
- const token = process.env.BEARER_TOKEN;
-  console.log ("global user Id",global.userID)
-  //########################################## 
- //This function will take the two variables and pass them to the Get RestAPI call 
-  myGetRestCall.getWithBearerToken(url, token)
-.then(data => { console.log("user ", data)
-  varSections = JSON.stringify(data.user.sections)  
-  varSections = JSON.parse(varSections)
-  varEducation = varSections.education 
-  varExperience = varSections.experience   
-  varVolunteering = varSections.volunteering     
-  varSkills = varSections.skills  
-  varAbout =  varSections.about 
-      
-      res.render('profile', { title: 'Profile Page', about:varAbout,education:varEducation,experience:varExperience,skills:varSkills,volunteering:varVolunteering});
-      }
-)
-.catch(error => console.error(error));
-    }
+  // this in you route 
+  const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/' + global.userID //- where the URL is whatever Get RestAPI Request  you are calling
+  const token = process.env.BEARER_TOKEN;
+   console.log ("global user Id",global.userID)
+   //########################################## 
+  //This function will take the two variables and pass them to the Get RestAPI call 
+   myGetRestCall.getWithBearerToken(url, token)
+ .then(data => { console.log("user ", data)
+   varSections = JSON.stringify(data.user.sections)  
+   varSections = JSON.parse(varSections)
+   varEducation = varSections.education 
+   varExperience = varSections.experience   
+   varVolunteering = varSections.volunteering     
+   varSkills = varSections.skills  
+   varAbout =  varSections.about 
+       
+       res.render('profile', { title: 'Profile Page', about:varAbout,education:varEducation,experience:varExperience,skills:varSkills,volunteering:varVolunteering});
+       }
+ )
+ .catch(error => console.error(error));
+     }
+  
+ );
+
+ /* GET users profile. */
+router.get('/:userName', function(req, res, next) {
+  // this in you route 
+  const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/' + req.params.userName //- where the URL is whatever Get RestAPI Request  you are calling
+  const token = process.env.BEARER_TOKEN;
+   console.log ("Username",req.params.userName)
+   //########################################## 
+  //This function will take the two variables and pass them to the Get RestAPI call 
+   myGetRestCall.getWithBearerToken(url, token)
+ .then(data => { console.log("user ", data)
+   varSections = JSON.stringify(data.user.sections)  
+   varSections = JSON.parse(varSections)
+   varEducation = varSections.education 
+   varExperience = varSections.experience   
+   varVolunteering = varSections.volunteering     
+   varSkills = varSections.skills  
+   varAbout =  varSections.about 
+       
+       res.render('profile', { title: 'Profile Page', about:varAbout,education:varEducation,experience:varExperience,skills:varSkills,volunteering:varVolunteering});
+       }
+ )
+ .catch(error => console.error(error));
+     }
+  
+ );
+
  
-);
-    
 module.exports = router;
