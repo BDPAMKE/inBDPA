49e6363d871a086ab249380f336d510edb4a5278
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2023-08-12 17:45:06 +0000

    role added to header - flexible

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const auth = require("../middleware/verifytoken");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'InBDPA Home' });
+router.get('/', auth, function (req, res, next) {
+  const role=res.locals.role;
+  const name=res.locals.result;
+  res.render('index', { title: 'InBDPA Home', role: role, name: name  });
 });
 
 module.exports = router;
